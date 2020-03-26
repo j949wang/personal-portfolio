3d6e68a688e6363fd92cb71fda8daa2e8fc4992a
Jiaxi Wang <Jiaxi.Wang@ibm.com@Jiaxis-MacBook-Pro.local>
2020-03-26 00:01:10 -0400

    Updates

---
+++ precache-manifest.552e282078ec5eaa42cea4128eca4797.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a779da3fc8f03d16f096ef15dc04efc",
+    "url": "/personal-portfolio/index.html"
+  },
+  {
+    "revision": "61d47966f17a8d3133ec",
+    "url": "/personal-portfolio/static/css/main.c9043838.chunk.css"
+  },
+  {
+    "revision": "79f491fa5e1c64ace6c1",
+    "url": "/personal-portfolio/static/js/2.0ab8c4b5.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/personal-portfolio/static/js/2.0ab8c4b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61d47966f17a8d3133ec",
+    "url": "/personal-portfolio/static/js/main.764cb2e5.chunk.js"
+  },
+  {
+    "revision": "fbd52b79a2256129d0f1",
+    "url": "/personal-portfolio/static/js/runtime-main.225001ee.js"
+  },
+  {
+    "revision": "ce819db2d09461b54cbc14417a81b0d0",
+    "url": "/personal-portfolio/static/media/CA_Glass_Logo_no_bg.ce819db2.png"
+  },
+  {
+    "revision": "331f3bbdbf5d6a3cd6ab3300f3557123",
+    "url": "/personal-portfolio/static/media/Scms_logo.331f3bbd.png"
+  },
+  {
+    "revision": "dd384b56a80590a30cce70896701600f",
+    "url": "/personal-portfolio/static/media/homedepot_logo.dd384b56.png"
+  },
+  {
+    "revision": "947daeeff9a5fe89b46f38a35fe61511",
+    "url": "/personal-portfolio/static/media/totoro-sketch-final.947daeef.png"
+  }
+]);
